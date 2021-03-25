c4532e814987bdb4b7070552da792cbe06e2e434
Djakson Cleber <djakson.filho@gmail.com>
2021-03-25 19:08:52 -0300

    Updates Database

---
+++ InvoiceManagerApp/Migrations/20210325210759_Add_Entities_To_Db.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceManagerApp.Migrations
+{
+    public partial class Add_Entities_To_Db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Rg = table.Column<string>(type: "varchar(50)", nullable: false),
+                    BancoNome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    BancoAgencia = table.Column<string>(type: "varchar(50)", nullable: false),
+                    BancoConta = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fornecedor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Vertical = table.Column<string>(type: "varchar(100)", nullable: false),
+                    LinkLogin = table.Column<string>(type: "varchar(MAX)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fatura",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DtEmissao = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DtVencimento = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal", nullable: false),
+                    ValorPago = table.Column<decimal>(type: "decimal", nullable: false),
+                    IdFornecedor = table.Column<int>(type: "int", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fatura", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fatura_Cliente_IdCliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Cliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Fatura_Fornecedor_IdFornecedor",
+                        column: x => x.IdFornecedor,
+                        principalTable: "Fornecedor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fatura_IdCliente",
+                table: "Fatura",
+                column: "IdCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fatura_IdFornecedor",
+                table: "Fatura",
+                column: "IdFornecedor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fatura");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Fornecedor");
+        }
+    }
+}
